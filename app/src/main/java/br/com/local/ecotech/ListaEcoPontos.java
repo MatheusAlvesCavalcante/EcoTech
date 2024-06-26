276c854a3753f3ccb8df39c095eff47955c14098
sosofisouza <sofiasouza0512@gmail.com>
2024-06-26 08:23:34 -0300

    inicio da tela lista

---
+++ app/src/main/java/br/com/local/ecotech/ListaEcoPontos.java
@@ -0,0 +1,15 @@
+package br.com.local.ecotech;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListaEcoPontos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lista);
+    }
+
+}
