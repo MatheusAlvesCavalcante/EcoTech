8b2d3e0fed58bd671608b4df5156eada744c195d
sosofisouza <sofiasouza0512@gmail.com>
2024-08-05 10:02:37 -0300

    tela bem vindos

---
+++ app/src/main/java/br/com/local/ecotech/BemVindo.java
@@ -0,0 +1,14 @@
+package br.com.local.ecotech;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class BemVindo extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bemvindo);
+    }
+}
