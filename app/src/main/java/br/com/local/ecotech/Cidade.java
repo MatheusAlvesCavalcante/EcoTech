1b966cc704ce2fc46467641e6fae7283627d7850
sosofisouza <sofiasouza0512@gmail.com>
2024-08-07 12:09:14 -0300

    testes

--- app/src/main/java/br/com/local/ecotech/Cidade.java
+++ app/src/main/java/br/com/local/ecotech/Cidade.java
@@ -2,24 +2,60 @@ package br.com.local.ecotech;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
 import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class Cidade extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        Spinner spinnerCidades = findViewById(R.id.spinner_cidades);
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.cidade);
 
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.cidades, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            Spinner spinner = findViewById(R.id.spinner);
+            ImageButton nextButton = findViewById(R.id.nextButton);
 
-        spinnerCidades.setAdapter(adapter);
+            // Crie um ArrayAdapter usando o array de strings e um layout padrão do Spinner
+            ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                    R.array.cities_array, android.R.layout.simple_spinner_item);
+
+            // Especifique o layout a ser usado quando a lista de escolhas aparecer
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+            // Aplique o adaptador ao Spinner
+            spinner.setAdapter(adapter);
+
+            // Adicione um listener ao Spinner
+            spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                    String city = parentView.getItemAtPosition(position).toString();
+                    Toast.makeText(parentView.getContext(), "Cidade selecionada: " + city, Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> parentView) {
+                    // Do nothing
+                }
+            });
+
+            // Adicione um listener ao botão de próximo
+            nextButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String selectedCity = spinner.getSelectedItem().toString();
+                    // Navegar para a próxima tela ou realizar alguma ação
+                    Toast.makeText(Cidade.this, "Próxima ação com a cidade: " + selectedCity, Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
     }
+
 }
 
