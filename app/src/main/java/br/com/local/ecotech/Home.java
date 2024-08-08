c8da6cf7da425eadee85b4892989a0baecfa14f4
EcoTechMobile <matheuscavalcantemt21@gmail.com>
2024-08-07 23:22:04 -0300

    Arrumei uns erros e linkei com as outras páginas

--- app/src/main/java/br/com/local/ecotech/Home.java
+++ app/src/main/java/br/com/local/ecotech/Home.java
@@ -2,7 +2,10 @@ package br.com.local.ecotech;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Home extends AppCompatActivity {
 
@@ -10,5 +13,39 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-    }
+        Button abrindo_tela_bem_vindo = findViewById(R.id.btn_bem_vindo);
+
+        abrindo_tela_bem_vindo.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(Home.this, BemVindo.class );
+                startActivity (intent);
+            }
+        });
+
+        Button abrindo_tela_lista = findViewById(R.id.btn_lista);
+
+        abrindo_tela_lista.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(Home.this, ListaEcoPontos.class );
+                startActivity (intent);
+            }
+        });
+
+    Button abrindo_tela_descarte = findViewById(R.id.btn_descarte);
+
+        abrindo_tela_descarte.setOnClickListener(new View.OnClickListener() {
+
+@Override
+public void onClick(View v){
+        Intent intent = new Intent(Home.this, DescarteResiduo.class );
+        startActivity (intent);
+        }
+        });
+        }
 }
+
+
