d8e44b5194d2cc49f099591b6db0faf09ced748f
EcoTechMobile <matheuscavalcantemt21@gmail.com>
2024-09-15 16:13:41 -0300

    linkei a primeira página e fiz a importancia

---
+++ app/src/main/java/br/com/local/ecotech/ImportanciaREC.java
@@ -0,0 +1,13 @@
+package br.com.local.ecotech;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ImportanciaREC extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.importancia_rec);
+    }
+}
--- app/src/main/java/br/com/local/ecotech/MainActivity.java
+++ app/src/main/java/br/com/local/ecotech/MainActivity.java
@@ -1,8 +1,11 @@
 package br.com.local.ecotech;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView imageView = findViewById(R.id.imageView2);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Home.class);
+                startActivity(intent);
+            }
+        });
+
 
     }
 }
