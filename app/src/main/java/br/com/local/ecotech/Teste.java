0edc78c3fee134c6c23380eb3688684bcb3be8ac
sosofisouza <sofiasouza0512@gmail.com>
2024-09-30 09:53:58 -0300

    .

---
+++ app/src/main/java/br/com/local/ecotech/Teste.java
@@ -0,0 +1,17 @@
+package br.com.local.ecotech;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Teste extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teste);
+
+
+
+    }
+}
